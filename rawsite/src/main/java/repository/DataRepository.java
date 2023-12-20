c1b87a6fc9b33b1a80b8e3e60d183ca9247d7f44
tenbullets <lnayc368@gmail.com>
2023-12-20 21:23:50 +0300

    added code

--- rawsite/src/main/java/repository/DataRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import javax.servlet.http.HttpServletResponse;
-
-public interface DataRepository {
-    void userSave(String username, String email, String password, String uuid);
-    void addCookieId(String uuid, HttpServletResponse response);
-}
--- rawsite/src/main/java/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import models.User;
-
-import java.util.List;
-
-public interface UsersRepository extends CrudRepository<User> {
-    List allUsers();
-}
